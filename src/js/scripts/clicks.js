51120e2e7ea7acc7b31fdc7e32777d8adf1f379e
DiMoNiO04 <dima.razumov.940@mail.ru>
2023-11-30 00:00:13 +0300

    feat: add section steps-investor

--- src/js/scripts/clicks.js
+++ src/js/scripts/clicks.js
@@ -1,24 +1,48 @@
 function clickFullText() {
-  if (document.querySelector('.js-dropdown-list') && window.innerWidth < 767) {
+  if (document.querySelector('.js-dropdown-list')) {
     const dropdownLists = document.querySelectorAll('.js-dropdown-list');
 
     dropdownLists.forEach((dropdownList) => {
       const dropdownTitle = dropdownList.querySelector('.js-dropdown-title');
       const dropdownContent = dropdownList.querySelector('.js-dropdown-content');
 
-      dropdownTitle.addEventListener('click', () => {
-        const contentHeight = dropdownContent.scrollHeight;
+      if (
+        (dropdownList.classList.contains('footer__list') && window.innerWidth < 768) ||
+        !dropdownList.classList.contains('footer__list')
+      ) {
+        dropdownTitle.addEventListener('click', () => {
+          const contentHeight = dropdownContent.scrollHeight;
 
-        if (dropdownList.classList.contains('active')) {
-          dropdownContent.style.height = '0px';
-        } else {
-          dropdownContent.style.height = `${contentHeight}px`;
-        }
+          if (dropdownList.classList.contains('active')) {
+            dropdownContent.style.height = '0px';
+          } else {
+            dropdownContent.style.height = `${contentHeight}px`;
+          }
 
-        dropdownList.classList.toggle('active');
-      });
+          dropdownList.classList.toggle('active');
+        });
+      }
     });
   }
 }
 
+function addActiveFirstStepsOnLoad() {
+  const dropdownLists = document.querySelectorAll('.js-dropdown-list');
+
+  let isFind = false;
+  dropdownLists.forEach((dropdownList) => {
+    if (dropdownList.classList.contains('steps-investor__item') && !isFind) {
+      const dropdownContent = dropdownList.querySelector('.js-dropdown-content');
+      const contentHeight = dropdownContent.scrollHeight;
+      dropdownContent.style.height = `${contentHeight}px`;
+      dropdownList.classList.add('active');
+      isFind = true;
+    }
+  });
+}
+
 clickFullText();
+
+document.addEventListener('DOMContentLoaded', () => {
+  addActiveFirstStepsOnLoad();
+});
