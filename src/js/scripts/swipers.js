1c363d13203cdc785bd7fab64da8e86b9982685d
DiMoNiO04 <dima.razumov.940@mail.ru>
2023-11-30 19:48:37 +0300

    feat: add success-hist section

--- src/js/scripts/swipers.js
+++ src/js/scripts/swipers.js
@@ -24,8 +24,32 @@ function initBenefitsSlider() {
   }
 }
 
+function initSuccessHistSlider() {
+  if (document.querySelector('.success-hist__swiper')) {
+    const slider = document.querySelector('.success-hist__swiper');
+    const fraction = slider.parentElement.querySelector('.slider-navigation__fraction');
+    const btnPrev = slider.parentElement.querySelector('.slider-navigation__btns-prev');
+    const btnNext = slider.parentElement.querySelector('.slider-navigation__btns-next');
+
+    new Swiper(slider, {
+      modules: [Pagination, Navigation],
+      slidesPerView: 1,
+      loop: true,
+      pagination: {
+        el: fraction,
+        type: 'fraction',
+      },
+      navigation: {
+        nextEl: btnNext,
+        prevEl: btnPrev,
+      },
+    });
+  }
+}
+
 function initSliders() {
   initBenefitsSlider();
+  initSuccessHistSlider();
 }
 
 document.addEventListener('DOMContentLoaded', initSliders);
