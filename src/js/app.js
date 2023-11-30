d805f96cb5136ddc835874a3ef9979e99f8a553b
DiMoNiO04 <dima.razumov.940@mail.ru>
2023-11-30 17:13:47 +0300

    feat: add benefits section

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,7 @@
 import * as flsFunctions from './modules/gulp.js';
 
+// Scripts
 import './scripts/clicks.js';
+import './scripts/swipers.js';
 
 flsFunctions.isWebp();
---
+++ src/js/scripts/swipers.js
@@ -0,0 +1,27 @@
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+
+function initBenefitsSlider() {
+  if (document.querySelector('.benefits__swiper')) {
+    const slider = document.querySelector('.benefits__swiper');
+    const fraction = slider.parentElement.querySelector('.slider-navigation__fraction');
+    const btnPrev = slider.parentElement.querySelector('.slider-navigation__btns-prev');
+    const btnNext = slider.parentElement.querySelector('.slider-navigation__btns-next');
+
+    new Swiper(slider, {
+      modules: [Pagination, Navigation],
+      slidesPerView: 1,
+      loop: true,
+      pagination: {
+        el: fraction,
+        type: 'fraction',
+      },
+      navigation: {
+        nextEl: btnNext,
+        prevEl: btnPrev,
+      },
+    });
+  }
+}
+
+initBenefitsSlider();
